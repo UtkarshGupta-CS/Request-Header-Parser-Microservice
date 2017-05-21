e720fba8ba7990d1da22981b3cc0e9655e61990e
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-05-21 15:43:43 +0530

    add res.send(req.ip)

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
 const express = require('express');
 let app = new express();
 const os = require('os');
-const publicIp = require('public-ip');
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-   /*let interfaces = os.networkInterfaces();
-   let addresses = [];
-   for (let k in interfaces) {
-      for (let k2 in interfaces[k]) {
-         let address = interfaces[k][k2];
-         if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
-         }
-      }
-   }
-   // console.log(addresses);
+
    // const software = new os.cpus();
    // const ip = os.networkInterfaces();
    // const host = os.constants;
    // console.log(software)
    // console.log(ip)
    // console.log(host)
-   console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress)*/
-   res.send(req.headers['x-forwarded-for'] || req.connection.remoteAddress)
+   
+   const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress ;
+   console.log(req.ip)
 
-   publicIp.v4().then(ip => {
-      console.log(ip);
-      // res.send(ip)
-      //=> '46.5.21.123'
-   });
-
-   /*publicIp.v6().then(ip => {
-      console.log(ip);
-      //=> 'fe80::200:f8ff:fe21:67cf'
-   });*/
-   // res.send('fuck u');
+   res.send(req.ip);
 })
 
 app.listen(port, (req, err) => {
